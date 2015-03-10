2fd96f8af04c6a08eb3db1137e0e2a8f40a089d8
Johan Sundell <johan@pixpro.net>
2015-03-10 10:16:44 +0100

    Made the test os agnostic

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -17,7 +18,7 @@ func TestFall_1A(t *testing.T) {
 		t.Errorf("Expeded %s, got %s", expect, result)
 	}
 
-	dat, _ := ioutil.ReadFile("files/testfall-1a.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "testfall-1a.txt")
 	result, _ = getHmacByte(dat)
 	if result != expect {
 		t.Errorf("Expeded %s, got %s", expect, result)
@@ -25,7 +26,7 @@ func TestFall_1A(t *testing.T) {
 }
 
 func TestFall_1B(t *testing.T) {
-	dat, _ := ioutil.ReadFile("files/testfall-1b.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "testfall-1b.txt")
 	expect := "9BA94363739D45256DF4B6FA3B9DE1CD"
 
 	result, err := getHmacByte(dat)
@@ -39,7 +40,7 @@ func TestFall_1B(t *testing.T) {
 }
 
 func TestFall_1C(t *testing.T) {
-	dat, _ := ioutil.ReadFile("files/testfall-1c.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "testfall-1c.txt")
 	expect := "826CA6CBA33F7E1D3CC9161A0956A35B"
 
 	result, err := getHmacByte(dat)
@@ -53,7 +54,7 @@ func TestFall_1C(t *testing.T) {
 }
 
 func TestFall_2A(t *testing.T) {
-	dat, _ := ioutil.ReadFile("files/hmac_testfall-2a.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "hmac_testfall-2a.txt")
 	expect := "9473EDFCAA8CD2434D6D76ABFFC991BD"
 
 	result, err := getHmacByte(dat)
@@ -67,7 +68,7 @@ func TestFall_2A(t *testing.T) {
 }
 
 func TestFall_2B(t *testing.T) {
-	dat, _ := ioutil.ReadFile("files/hmac_testfall-2b.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "hmac_testfall-2b.txt")
 	expect := "20956E44B404C4085446139B2B952D77"
 
 	result, err := getHmacByte(dat)
@@ -81,7 +82,7 @@ func TestFall_2B(t *testing.T) {
 }
 
 func TestFall_2C(t *testing.T) {
-	dat, _ := ioutil.ReadFile("files/hmac_testfall-2c.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "hmac_testfall-2c.txt")
 	expect := "515704694958361678194D51850FF157"
 
 	result, err := getHmacByte(dat)
@@ -95,7 +96,7 @@ func TestFall_2C(t *testing.T) {
 }
 
 func TestFall_2D(t *testing.T) {
-	dat, _ := ioutil.ReadFile("files/hmac_testfall-2d.txt")
+	dat, _ := ioutil.ReadFile("files" + string(os.PathSeparator) + "hmac_testfall-2d.txt")
 	expect := "68C5954818010DDDCB1EC02963E8C123"
 
 	result, err := getHmacByte(dat)
